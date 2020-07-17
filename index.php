7ff9f3136ac544c92bbbeb3ecb9a06e83c31ff2a
murillo <murillo@murillo.lan>
2020-07-17 17:16:50 -0300

    ok

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//echo phpinfo();
+
+
+$userLinux = "fulano";
+
+echo "<p> o Usuário é {$userLinux} </p>";
+
+
+$server = [];
+
+$server = [
+
+   "ip" => "192.168.0.1",
+   "port" => 3326,
+   "dns" => "8.8.8."	 		
+
+
+];
+
+var_dump(
+$server
+);
